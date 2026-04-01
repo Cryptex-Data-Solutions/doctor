64aa47db8b1073d9be3d683b66a0122dfd323f6b
Bradley Goulding <bradley@cryptex.co.za>
2026-04-01 12:42:34 +0200

    fix: use outputPath instead of undefined newPath in prepare-sample.js

--- scripts/prepare-sample.js
+++ scripts/prepare-sample.js
@@ -7,7 +7,7 @@ const __dirname = path.dirname(__filename);
 
 const outputPath = path.join(__dirname, `../doctor-sample/doctor.json`);
 
-const file = fs.readFileSync(newPath, { encoding: "utf-8" });
+const file = fs.readFileSync(outputPath, { encoding: "utf-8" });
 if (file) {
   const data = JSON.parse(file);
 
@@ -17,7 +17,7 @@ if (file) {
 
   if (data && data.multilingual && data.multilingual.translator) {
     data.multilingual.translator.key = process.env.TRANSLATOR_KEY;
-    fs.writeFileSync(newPath, JSON.stringify(data, null, 2), {
+    fs.writeFileSync(outputPath, JSON.stringify(data, null, 2), {
       encoding: "utf-8",
     });
   }
